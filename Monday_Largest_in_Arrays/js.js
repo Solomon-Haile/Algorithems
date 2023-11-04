7e8b84663c028e0706a56cc48c12c402795c2cf2
Solomon Haile <solexymahtot@gmail.com>
2023-11-04 08:17:31 +0100

    subarray max commit

---
+++ Monday_Largest_in_Arrays/js.js
@@ -0,0 +1,28 @@
+// finding the largest of arrays
+
+// use for loop
+// access nested array
+// push large numner 
+
+const largestOfFour = (arr) => {
+    const newArr = [];
+    
+
+    for(let i = 0; i < arr.length; i++) {
+        let max = arr[i][0]
+
+        for(let j = 1; j < arr[i].length; j++) {
+
+            if(arr[i][j] > max) {
+                max = arr[i][j]; // update largest value
+            }
+        }
+
+        newArr.push(max);
+    }
+    return newArr;
+}
+
+let sampleArray = [[4, 5, 1, 3], [13, 27, 18, 26], [32, 35, 37, 39], [1000, 1001, 857, 1]]
+
+console.log(largestOfFour(sampleArray));
