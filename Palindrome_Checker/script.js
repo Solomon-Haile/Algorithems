b77d4112aa6f63906a2f6f0ea58f17ce8d025fb4
Solomon Haile <solexymahtot@gmail.com>
2023-11-05 11:35:37 +0100

    some updates

--- Palindrome_Checker/script.js
+++ Palindrome_Checker/script.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 // this is Thursday's palindrome checker assignment
 
 const isPalindrome = (str) => {
@@ -10,17 +9,4 @@ const isPalindrome = (str) => {
     return true;
 }
 
-=======
-// this is Thursday's palindrome checker assignment
-
-const isPalindrome = (str) => {
-    for(let i = 0; i < str.length / 2; i++) {
-        if(str[i] !== str[str.length - 1 - i]) {
-            return false;
-        }
-    }
-    return true;
-}
-
->>>>>>> d8618ae397a8e8297cb0767a0f8c8516be57816e
 //console.log(isPalindrome()); 
